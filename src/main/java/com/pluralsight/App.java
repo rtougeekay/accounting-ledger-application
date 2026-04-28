1c80be9f5df358814a5228fa85864b2714b67c71
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-04-28 13:21:19 -0500

    array list will be shown for transactions

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -2,7 +2,9 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -80,6 +82,37 @@ public class App {
         } catch (Exception e) {
             System.out.println("Something went wrong. Transaction was not saved.");
         }
+    }
+
+    public static ArrayList<Transaction> loadTransactions() {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] parts = line.split("\\|");
+
+                String date = parts[0];
+                String time = parts[1];
+                String description = parts[2];
+                String vendor = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+
+                Transaction transaction = new Transaction(date, time, description, vendor, amount);
+                transactions.add(transaction);
+
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Could not read transactions file.");
+        }
+
+        return transactions;
     }
 
 
